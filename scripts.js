a9ad8f5bdc6ef537a109c205a8269401edc51c78
Alex <espia816@hsd.k12.or.us>
2022-05-10 14:20:19 -0700

    Move js, add js commands

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
-// Test Scripts
-
+// Create a paragraph tag
+var paragraph = document.createElement('p');
+// Add text to paragraph
+paragraph.textContent = 'The DOM is the Bomb!';
+// Add text to body
+document.body.appendChild(paragraph);
+// Create DIV Elemnt
+var div = document.createElement('DIV');
+// Set Height of Element
+div.style.height = '100vh';
+// Append Element to DOM
+document.body.appendChild(div);
+// Add Even Listener to Element
+div.addEventListener('mousemove', function(event) {
+  console.log(event);
+  var x = event.clientX;
+  var y = event.clientY;
+  div.textContent = x + ',' + y;
+  div.style.backgroundColor = 'rgb(' + x + ',' + y + ', 100)';
+});
